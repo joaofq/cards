02ee0f498054df81cd5c01fcdc7f73b2bb47288d
João Fernando <joaofq@hotmail.com>
2023-01-17 09:05:56 -0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
