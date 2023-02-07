f2e0b20f63a7f75bbdc13ff4adb7d24f3c18b1ea
João Fernando <joaofq@hotmail.com>
2023-02-06 21:28:13 -0300

    Change API to Async/await functions

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,28 +1,44 @@
-export function getDeck() {
-  return fetch(
-    'https://www.deckofcardsapi.com/api/deck/new/shuffle/?cards=2s,3s,4s,5s,6s,7s,8s,9s,0s',
-  ).then((res) => {
+export async function getDeck() {
+  try {
+    const res = await fetch(
+      'https://www.deckofcardsapi.com/api/deck/new/shuffle/?cards=2s,3s,4s,5s,6s,7s,8s,9s,0s',
+    );
     if (res.ok) {
-      return res.json();
+      return await res.json();
     } else {
-      return Promise.reject('error: ' + res.status);
+      throw new Error('error: ' + res.status);
     }
-  });
+  } catch (error) {
+    return error;
+  }
 }
 
-export function drawCard(deckId) {
-  return fetch(
-    `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`,
-  ).then((res) => {
+export async function drawCard(deckId) {
+  try {
+    const res = await fetch(
+      `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`,
+    );
     if (res.ok) {
-      return res.json();
+      return await res.json();
     } else {
-      return Promise.reject('error: ' + res.status);
+      throw new Error('error: ' + res.status);
     }
-  });
+  } catch (error) {
+    return error;
+  }
 }
 
-export function shuffleDeck(deckId) {
-  return fetch(`https://deckofcardsapi.com/api/deck/${deckId}/shuffle/
-  `);
+export async function shuffleDeck(deckId) {
+  try {
+    const res = await fetch(
+      `https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`,
+    );
+    if (res.ok) {
+      return await res.json();
+    } else {
+      throw new Error('error: ' + res.status);
+    }
+  } catch (error) {
+    return error;
+  }
 }
